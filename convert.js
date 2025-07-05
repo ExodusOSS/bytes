ecb847691a4f08cb8489c24e3861b1b86a43b002
Nikita Skovoroda <chalkerx@gmail.com>
2025-07-05 12:49:14 +0400

    feat: add /convert

---
+++ convert.js
@@ -0,0 +1,75 @@
+import { assert, assertUint8 } from './assert.js'
+
+// TODO: make Buffer optional
+
+// base64:    A-Za-z0-9+/ and =
+// base64url: A-Za-z0-9_-
+
+// From Uint8Array or a Buffer, defaults to uint8
+// NOTE: base64url omits padding
+export function fromUint8Super(arr, format = 'uint8') {
+  assertUint8(arr)
+  switch (format) {
+    case 'uint8':
+      if (arr.constructor === Uint8Array) return arr // fast path
+      return new Uint8Array(arr.buffer, arr.byteOffset, arr.byteLength)
+    case 'buffer':
+      if (arr.constructor === Buffer && Buffer.isBuffer(arr)) return arr
+      return Buffer.from(arr.buffer, arr.byteOffset, arr.byteLength)
+    case 'hex':
+      if (Uint8Array.prototype.toHex && arr.toHex === Uint8Array.prototype.toHex) return arr.toHex()
+      if (arr.constructor === Buffer && Buffer.isBuffer(arr)) return arr.toString(format)
+      return Buffer.from(arr.buffer, arr.byteOffset, arr.byteLength).toString(format)
+    case 'base64':
+      if (Uint8Array.prototype.toBase64 && arr.toBase64 === Uint8Array.prototype.toBase64) {
+        return arr.toBase64()
+      }
+
+      if (arr.constructor === Buffer && Buffer.isBuffer(arr)) return arr.toString(format)
+      return Buffer.from(arr.buffer, arr.byteOffset, arr.byteLength).toString(format)
+    case 'base64url':
+      if (Uint8Array.prototype.toBase64 && arr.toBase64 === Uint8Array.prototype.toBase64) {
+        return arr.toBase64({ alphabet: 'base64url', omitPadding: true })
+      }
+
+      return fromUint8Super(arr, 'base64')
+        .replaceAll('+', '-')
+        .replaceAll('/', '_')
+        .replaceAll('=', '')
+  }
+
+  throw new TypeError('Unexpected format')
+}
+
+// Unlike Buffer.from(), throws on invalid input
+export function fromHex(arg, format = 'uint8') {
+  if (Uint8Array.fromHex) return fromUint8Super(Uint8Array.fromHex(arg), format)
+  if (typeof arg !== 'string') throw new TypeError('Input is not a string')
+  assert(arg.length % 2 === 0 && !/[^0-9a-f]/ui.test(arg), 'Input is not a hex string')
+  return fromUint8Super(Buffer.from(arg, 'hex'), format)
+}
+
+// Unlike Buffer.from(), throws on invalid input (non-base64 symbols and incomplete chunks)
+// Unlike Buffer.from() and Uint8Array.fromBase64(), does not allow spaces
+// Unlike Uint8Array.fromBase64(), accepts both base64 and base64url
+// TODO: add a strict mode? (we allow overflow by default, like VR==)
+// TODO: add 'alphabet' option to enforce input format?
+export function fromBase64(arg, format = 'uint8') {
+  if (typeof arg !== 'string') throw new TypeError('Input is not a string')
+  if (Uint8Array.fromBase64) {
+    if (!/[^0-9a-z=+/]/ui.test(arg)) return fromUint8Super(Uint8Array.fromBase64(arg), format)
+    if (/[^0-9a-z_-]/ui.test(arg)) throw new Error('Invalid character in base64/base64url input')
+    return fromUint8Super(Uint8Array.fromBase64(arg, { alphabet: 'base64url' }), format)
+  }
+
+  assert(!/=[^=]/ui.test(arg), 'Invalid input after base64 padding')
+  assert(arg.length % 4 !== 1, 'Invalid base64 length')
+  if (arg.endsWith('=')) {
+    assert(arg.length % 4 === 0, 'Invalid padded base64 length')
+    assert(arg[arg.length - 3] !== '=', 'Excessive padding')
+  }
+
+  if (!/[^0-9a-z=+/]/ui.test(arg)) return fromUint8Super(Buffer.from(arg, 'base64'), format) // base64
+  if (!/[^0-9a-z_-]/ui.test(arg)) return fromUint8Super(Buffer.from(arg, 'base64'), format) // base64url
+  throw new Error('Invalid character in base64/base64url input')
+}
--- src/base.js
+++ src/base.js
@@ -1,29 +1,5 @@
-import { assertUint8 } from '../assert.js'
-
-function toBuffer(arg) {
-  assertUint8(arg)
-  return Buffer.isBuffer(arg) ? arg : Buffer.from(arg.buffer, arg.byteOffset, arg.byteLength)
-}
-
-function fromBuffer(x, format) {
-  assert(Buffer.isBuffer(x)) // internal use only
-  switch (format) {
-    case 'uint8': return new Uint8Array(x.buffer, x.byteOffset, x.byteLength)
-    case 'buffer': return x
-    default: throw new Error('Unexpected format')
-  }
-}
-
-export const base64 = (arg) => toBuffer(arg).toString('base64')
-
 export function base32url(arg) {
   throw new Error('Unimplemented')
-}
-
-export const base64Parse = (arg, format) => {
-  assert(!/[A-Za-z0-9+/=_-]/u, 'Invalid character in base64/base64url input')
-  // TODO: check that = are at the end only
-  return fromBuffer(Buffer.from(arg, 'base64'), format)
 }
 
 export function base32(arg) {
