02b87fe0b23b847662fcd1287dc886e9d15e79b5
Nikita Skovoroda <chalkerx@gmail.com>
2025-08-31 16:06:59 +0400

    Buffer is optional

--- convert.js
+++ convert.js
@@ -1,6 +1,10 @@
 import { assert, assertUint8 } from './assert.js'
 
-// TODO: make Buffer optional
+const { Buffer } = globalThis // Buffer is optional, only used when native
+const haveNativeBuffer = Buffer && !Buffer.TYPED_ARRAY_SUPPORT
+
+let hexArray
+let hexMap
 
 // From Uint8Array or a Buffer, defaults to uint8
 export function fromUint8Super(arr, format = 'uint8') {
@@ -14,8 +18,13 @@ export function fromUint8Super(arr, format = 'uint8') {
       return Buffer.from(arr.buffer, arr.byteOffset, arr.byteLength)
     case 'hex':
       if (Uint8Array.prototype.toHex && arr.toHex === Uint8Array.prototype.toHex) return arr.toHex()
-      if (arr.constructor === Buffer && Buffer.isBuffer(arr)) return arr.toString('hex')
-      return Buffer.from(arr.buffer, arr.byteOffset, arr.byteLength).toString('hex')
+      if (haveNativeBuffer) {
+        if (arr.constructor === Buffer && Buffer.isBuffer(arr)) return arr.toString('hex')
+        return Buffer.from(arr.buffer, arr.byteOffset, arr.byteLength).toString('hex')
+      }
+      // TODO: measure perf/optimize?
+      if (!hexArray) hexArray = Array.from({ length: 256 }, (_, i) => i.toString(16).padStart(2, '0'))
+      return Array.from({ length: arr.length }, (_, i) => hexArray[arr[i]]).join('')
   }
 
   throw new TypeError('Unexpected format')
@@ -26,5 +35,14 @@ export function fromHex(arg, format = 'uint8') {
   if (Uint8Array.fromHex) return fromUint8Super(Uint8Array.fromHex(arg), format)
   if (typeof arg !== 'string') throw new TypeError('Input is not a string')
   assert(arg.length % 2 === 0 && !/[^0-9a-f]/ui.test(arg), 'Input is not a hex string')
-  return fromUint8Super(Buffer.from(arg, 'hex'), format)
+  if (haveNativeBuffer) return fromUint8Super(Buffer.from(arg, 'hex'), format)
+  // TODO: measure perf/optimize?
+  if (!hexMap) {
+    hexMap = Object.create(null)
+    for (let i = 0; i < 256; i++) hexMap[i.toString(16).padStart(2, '0')] = i
+  }
+  const length = arg.length / 2
+  arg = arg.toLowerCase()
+  const arr = Uint8Array.from({ length }, (_, i) => hexMap[arg[2 * i] + arg[2 * i + 1]])
+  return fromUint8Super(arr, format)
 }
