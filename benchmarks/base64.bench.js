e4a6e5020dfe638649406bbe8786d7ac1eeee4e9
Nikita Skovoroda <chalkerx@gmail.com>
2025-09-13 16:03:10 +0400

    benchmark updates, partial

--- benchmarks/base64.bench.js
+++ benchmarks/base64.bench.js
@@ -4,38 +4,60 @@ import base64js from 'base64-js'
 import buffer from 'buffer/index.js'
 
 import { bufs } from './random.js'
+import { describe, test } from 'node:test'
 
 if (!globalThis.Buffer) globalThis.Buffer = buffer.Buffer
-Buffer.TYPED_ARRAY_SUPPORT = true
-const exodusPure = await import('../base64.js?pure') // eslint-disable-line @exodus/import/no-unresolved
-delete Buffer.TYPED_ARRAY_SUPPORT
 
-const val = exodus.toBase64(bufs[0])
-if (scureBase64.encode(bufs[0]) !== val) throw new Error('scure.base64')
-if (exodusPure.toBase64(bufs[0]) !== val) throw new Error('exodus pure')
+describe('benchmarks', async () => {
+  let exodusPure
+  if (!Buffer.TYPED_ARRAY_SUPPORT) {
+    Buffer.TYPED_ARRAY_SUPPORT = true
+    exodusPure = await import('../base64.js?pure') // eslint-disable-line @exodus/import/no-unresolved
+    delete Buffer.TYPED_ARRAY_SUPPORT
+  }
 
-for (let i = 0; i < 5; i++) {
-  console.time('base64-js')
-  for (const buf of bufs) base64js.fromByteArray(buf)
-  console.timeEnd('base64-js')
+  test('toBase64 coherence', (t) => {
+    const val = exodus.toBase64(bufs[0])
+    t.assert.equal(base64js.fromByteArray(bufs[0]), val, 'base64-js')
+    if (exodusPure) t.assert.equal(exodusPure.toBase64(bufs[0]), val, 'exodus pure')
+    t.assert.equal(Buffer.from(bufs[0]).toString('base64'), val, 'Buffer.from')
+    t.assert.equal(buffer.Buffer.from(bufs[0]).toString('base64'), val, 'buffer/Buffer.from')
+    t.assert.equal(scureBase64.encode(bufs[0]), val, 'scure.base64')
+  })
 
-  console.time('@exodus/bytes/base64.js')
-  for (const buf of bufs) exodus.toBase64(buf)
-  console.timeEnd('@exodus/bytes/base64.js')
+  for (let i = 0; i < 5; i++) {
+    test('toBase64', () => {
+      console.time('base64-js')
+      for (const buf of bufs) base64js.fromByteArray(buf)
+      console.timeEnd('base64-js')
 
-  console.time('@exodus/bytes/base64.js, pure')
-  for (const buf of bufs) exodusPure.toBase64(buf)
-  console.timeEnd('@exodus/bytes/base64.js, pure')
+      console.time('@exodus/bytes/base64.js')
+      for (const buf of bufs) exodus.toBase64(buf)
+      console.timeEnd('@exodus/bytes/base64.js')
 
-  console.time('@scure/base')
-  for (const buf of bufs) scureBase64.encode(buf)
-  console.timeEnd('@scure/base')
+      if (exodusPure) {
+        console.time('@exodus/bytes/base64.js, pure')
+        for (const buf of bufs) exodusPure.toBase64(buf)
+        console.timeEnd('@exodus/bytes/base64.js, pure')
+      }
 
-  console.time('Buffer.from')
-  for (const buf of bufs) Buffer.from(buf).toString('base64')
-  console.timeEnd('Buffer.from')
+      console.time('Buffer.from, zero-copy')
+      for (const buf of bufs) Buffer.from(buf.buffer, buf.byteOffset, buf.length).toString('base64')
+      console.timeEnd('Buffer.from, zero-copy')
 
-  console.time('buffer/Buffer.from')
-  for (const buf of bufs) buffer.Buffer.from(buf).toString('base64')
-  console.timeEnd('buffer/Buffer.from')
-}
+      console.time('Buffer.from')
+      for (const buf of bufs) Buffer.from(buf).toString('base64')
+      console.timeEnd('Buffer.from')
+
+      if (buffer.Buffer !== Buffer) {
+        console.time('buffer/Buffer.from')
+        for (const buf of bufs) buffer.Buffer.from(buf).toString('base64')
+        console.timeEnd('buffer/Buffer.from')
+      }
+
+      console.time('@scure/base')
+      for (const buf of bufs) scureBase64.encode(buf)
+      console.timeEnd('@scure/base')
+    })
+  }
+})
