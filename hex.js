3d04dbe6964dd357879c5a3517d917a0329ff90f
Nikita Skovoroda <chalkerx@gmail.com>
2025-09-13 14:46:57 +0400

    cleanup

--- hex.js
+++ hex.js
@@ -3,15 +3,15 @@ import { fromTypedArray } from './array.js'
 
 const { Buffer } = globalThis // Buffer is optional, only used when native
 const haveNativeBuffer = Buffer && !Buffer.TYPED_ARRAY_SUPPORT
+const { toHex: webHex } = Uint8Array.prototype // Modern engines have this
 
 let hexArray
 let dehexArray
 
 export function toHex(arr) {
   assertTypedArray(arr)
-  const u8 =
-    arr instanceof Uint8Array ? arr : new Uint8Array(arr.buffer, arr.byteOffset, arr.byteLength)
-  if (Uint8Array.prototype.toHex && u8.toHex === Uint8Array.prototype.toHex) return u8.toHex()
+  if (!(arr instanceof Uint8Array)) arr = new Uint8Array(arr.buffer, arr.byteOffset, arr.byteLength)
+  if (webHex && arr.toHex === webHex) return arr.toHex()
   if (haveNativeBuffer) {
     if (arr.constructor === Buffer && Buffer.isBuffer(arr)) return arr.toString('hex')
     return Buffer.from(arr.buffer, arr.byteOffset, arr.byteLength).toString('hex')
@@ -19,35 +19,41 @@ export function toHex(arr) {
 
   if (!hexArray) hexArray = Array.from({ length: 256 }, (_, i) => i.toString(16).padStart(2, '0'))
   let out = ''
-  for (let i = 0; i < arr.length; i++) out += hexArray[u8[i]]
+  for (let i = 0; i < arr.length; i++) out += hexArray[arr[i]]
   return out
 }
 
 // Unlike Buffer.from(), throws on invalid input
-export function fromHex(str, format = 'uint8') {
-  if (Uint8Array.fromHex) return fromTypedArray(Uint8Array.fromHex(str), format)
-  if (typeof str !== 'string') throw new TypeError('Input is not a string')
-  assert(str.length % 2 === 0, 'Input is not a hex string')
-  if (haveNativeBuffer) {
-    assert(!/[^0-9a-f]/iu.test(str), 'Input is not a hex string')
-    return fromTypedArray(Buffer.from(str, 'hex'), format)
-  }
-
-  if (!dehexArray) {
-    dehexArray = new Array(103) // f is 102
-    for (let i = 0; i < 16; i++) {
-      const s = i.toString(16)
-      dehexArray[s.charCodeAt(0)] = dehexArray[s.toUpperCase().charCodeAt(0)] = i
+let fromHex
+if (Uint8Array.fromHex) {
+  fromHex = (str, format = 'uint8') => fromTypedArray(Uint8Array.fromHex(str), format)
+} else {
+  fromHex = (str, format = 'uint8') => {
+    if (typeof str !== 'string') throw new TypeError('Input is not a string')
+    assert(str.length % 2 === 0, 'Input is not a hex string')
+    if (haveNativeBuffer) {
+      assert(!/[^0-9a-f]/iu.test(str), 'Input is not a hex string')
+      return fromTypedArray(Buffer.from(str, 'hex'), format)
     }
-  }
 
-  const arr = new Uint8Array(str.length / 2)
-  let j = 0
-  for (let i = 0; i < arr.length; i++) {
-    const a = dehexArray[str.charCodeAt(j++)] * 16 + dehexArray[str.charCodeAt(j++)]
-    if (!a && Number.isNaN(a)) throw new Error('Input is not a hex string')
-    arr[i] = a
-  }
+    if (!dehexArray) {
+      dehexArray = new Array(103) // f is 102
+      for (let i = 0; i < 16; i++) {
+        const s = i.toString(16)
+        dehexArray[s.charCodeAt(0)] = dehexArray[s.toUpperCase().charCodeAt(0)] = i
+      }
+    }
 
-  return fromTypedArray(arr, format)
+    const arr = new Uint8Array(str.length / 2)
+    let j = 0
+    for (let i = 0; i < arr.length; i++) {
+      const a = dehexArray[str.charCodeAt(j++)] * 16 + dehexArray[str.charCodeAt(j++)]
+      if (!a && Number.isNaN(a)) throw new Error('Input is not a hex string')
+      arr[i] = a
+    }
+
+    return fromTypedArray(arr, format)
+  }
 }
+
+export { fromHex }
