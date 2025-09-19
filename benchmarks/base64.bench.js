f5901c68056d3373bfd9628cd363b6a4dd9b6cb6
Nikita Skovoroda <chalkerx@gmail.com>
2025-09-19 18:07:09 +0400

    bench: add proper base64 benchmarks

--- benchmarks/base64.bench.js
+++ benchmarks/base64.bench.js
@@ -1,5 +1,6 @@
 import * as exodus from '@exodus/bytes/base64.js'
-import { base64 as scureBase64 } from '@scure/base'
+import { benchmark } from '@exodus/test/benchmark'
+import { base64 as scure } from '@scure/base'
 import base64js from 'base64-js'
 import buffer from 'buffer/index.js'
 
@@ -8,56 +9,94 @@ import { describe, test } from 'node:test'
 
 if (!globalThis.Buffer) globalThis.Buffer = buffer.Buffer
 
-describe('benchmarks', async () => {
-  let exodusPure
+describe('benchmarks: base64', async () => {
+  let scureJS // Fallback without Uint8Array.fromBase64, Uint8Array#toBase64
+  let exodusA // Fallback without Uint8Array.fromBase64, Uint8Array#toBase64
+  let exodusB // Fallback without native Buffer
+  let exodusC // Fallback without atob
+
+  const reset = []
+  if (Uint8Array.fromBase64 || Uint8Array.prototype.toBase64) {
+    const { fromBase64 } = Uint8Array
+    delete Uint8Array.fromBase64
+    reset.push(() => (Uint8Array.fromBase64 = fromBase64))
+    const { toBase64 } = Uint8Array.prototype
+    Uint8Array.prototype.toBase64 = undefined
+    reset.push(() => (Uint8Array.prototype.toBase64 = toBase64))
+    exodusA = await import('../base64.js?a') // eslint-disable-line @exodus/import/no-unresolved
+    scureJS = (await import('@scure/base?a')).base64 // eslint-disable-line @exodus/import/no-unresolved
+  }
+
   if (!Buffer.TYPED_ARRAY_SUPPORT) {
     Buffer.TYPED_ARRAY_SUPPORT = true
-    exodusPure = await import('../base64.js?pure') // eslint-disable-line @exodus/import/no-unresolved
-    delete Buffer.TYPED_ARRAY_SUPPORT
+    reset.push(() => delete delete Buffer.TYPED_ARRAY_SUPPORT)
+    exodusB = await import('../base64.js?b') // eslint-disable-line @exodus/import/no-unresolved
   }
+
+  if (globalThis.atob) {
+    const { atob } = globalThis
+    delete globalThis.atob
+    exodusC = await import('../base64.js?c') // eslint-disable-line @exodus/import/no-unresolved
+    globalThis.atob = atob
+  }
+
+  for (const f of reset) f()
+
+  const strings = bufs.map((x) => exodus.toBase64(x))
+
+  // [name, impl, skip]
+  const toBuf = (x, B) => B.from(x.buffer, x.byteOffet, x.byteLength)
+  const toBase64 = [
+    ['@exodus/bytes/base64', (x) => exodus.toBase64(x)],
+    ['@exodus/bytes/base64, no native', (x) => exodusA.toBase64(x), !exodusA],
+    ['@exodus/bytes/base64, no Buffer', (x) => exodusB.toBase64(x), !exodusB],
+    ['Buffer', (x) => toBuf(x, Buffer).toString('base64')],
+    ['Buffer.from', (x) => Buffer.from(x).toString('base64')],
+    ['buffer/Buffer', (x) => toBuf(x, buffer.Buffer).toString('base64'), Buffer === buffer.Buffer],
+    ['buffer/Buffer.from', (x) => buffer.Buffer.from(x).toString('base64'), Buffer === buffer.Buffer],
+    ['base64-js', (x) => base64js.fromByteArray(x)],
+    ['scure.base64', (x) => scure.encode(x)],
+    ['scure.base64, no native', (x) => scureJS.encode(x), !scureJS],
+  ]
+
+  // [name, impl, skip]
+  const fromBase64 = [
+    ['@exodus/bytes/base64', (x) => exodus.fromBase64(x)],
+    ['@exodus/bytes/base64, no native', (x) => exodusA.fromBase64(x), !exodusA],
+    ['@exodus/bytes/base64, no Buffer', (x) => exodusB.fromBase64(x), !exodusB],
+    ['@exodus/bytes/base64, no atob', (x) => exodusC.fromBase64(x), !exodusC],
+    ['Buffer.from', (x) => Buffer.from(x, 'base64')],
+    ['buffer/Buffer.from', (x) => buffer.Buffer.from(x, 'base64'), Buffer === buffer.Buffer],
+    ['base64-js', (x) => base64js.toByteArray(x)],
+    ['scure.base64', (x) => scure.decode(x)],
+    ['scure.base64, no native', (x) => scureJS.decode(x), !scureJS],
+  ]
 
   test('toBase64 coherence', (t) => {
-    const val = exodus.toBase64(bufs[0])
-    t.assert.equal(base64js.fromByteArray(bufs[0]), val, 'base64-js')
-    if (exodusPure) t.assert.equal(exodusPure.toBase64(bufs[0]), val, 'exodus pure')
-    t.assert.equal(Buffer.from(bufs[0]).toString('base64'), val, 'Buffer.from')
-    t.assert.equal(buffer.Buffer.from(bufs[0]).toString('base64'), val, 'buffer/Buffer.from')
-    t.assert.equal(scureBase64.encode(bufs[0]), val, 'scure.base64')
+    for (let i = 0; i < 100; i++) {
+      for (const [name, f, skip] of toBase64) {
+        if (!skip) t.assert.deepEqual(f(bufs[i]), strings[i], name)
+      }
+    }
   })
 
-  for (let i = 0; i < 5; i++) {
-    test('toBase64', () => {
-      console.time('base64-js')
-      for (const buf of bufs) base64js.fromByteArray(buf)
-      console.timeEnd('base64-js')
-
-      console.time('@exodus/bytes/base64.js')
-      for (const buf of bufs) exodus.toBase64(buf)
-      console.timeEnd('@exodus/bytes/base64.js')
-
-      if (exodusPure) {
-        console.time('@exodus/bytes/base64.js, pure')
-        for (const buf of bufs) exodusPure.toBase64(buf)
-        console.timeEnd('@exodus/bytes/base64.js, pure')
+  test('toBase64', { timeout: 10_000 }, async () => {
+    for (const [name, f, skip] of toBase64) {
+      await benchmark(`toBase64: ${name}`, { skip, args: bufs }, f)
+    }
+  })
+  
+  test('fromBase64 coherence', (t) => {
+    for (let i = 0; i < 100; i++) {
+      for (const [name, f, skip] of fromBase64) {
+        if (!skip) t.assert.deepEqual(f(strings[i]), bufs[i], name)
       }
+    }
+  })
 
-      console.time('Buffer.from, zero-copy')
-      for (const buf of bufs) Buffer.from(buf.buffer, buf.byteOffset, buf.length).toString('base64')
-      console.timeEnd('Buffer.from, zero-copy')
-
-      console.time('Buffer.from')
-      for (const buf of bufs) Buffer.from(buf).toString('base64')
-      console.timeEnd('Buffer.from')
-
-      if (buffer.Buffer !== Buffer) {
-        console.time('buffer/Buffer.from')
-        for (const buf of bufs) buffer.Buffer.from(buf).toString('base64')
-        console.timeEnd('buffer/Buffer.from')
-      }
-
-      console.time('@scure/base')
-      for (const buf of bufs) scureBase64.encode(buf)
-      console.timeEnd('@scure/base')
-    })
-  }
+  test('fromBase64', { timeout: 10_000 }, async () => {
+    for (const [name, f, skip] of fromBase64) {
+      await benchmark(`fromBase64: ${name}`, { skip, args: strings }, f)
+    }
+  })
 })
