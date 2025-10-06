febd395b6c3f0ed3e9f52be9a77dcb6a282e300d
Nikita Skovoroda <chalkerx@gmail.com>
2025-10-06 05:25:00 +0300

    fix: do not trim bom on fromBase64/32/Hex

--- fallback/_utils.js
+++ fallback/_utils.js
@@ -1,5 +1,6 @@
 const { Buffer, TextEncoder, TextDecoder } = globalThis
 const haveNativeBuffer = Buffer && !Buffer.TYPED_ARRAY_SUPPORT
 const isNative = (x) => x && (haveNativeBuffer || `${x}`.includes('[native code]')) // we consider Node.js TextDecoder/TextEncoder native
-export const nativeEncoder = isNative(TextEncoder) ? new TextEncoder() : null
-export const nativeDecoder = isNative(TextDecoder) ? new TextDecoder() : null
+const nativeEncoder = isNative(TextEncoder) ? new TextEncoder() : null
+const nativeDecoder = isNative(TextDecoder) ? new TextDecoder('utf8', { ignoreBOM: true }) : null
+export { nativeEncoder, nativeDecoder }
