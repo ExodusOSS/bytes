06c8b8afc84a5296f3725a624165fedeb5fc2bdf
Nikita Skovoroda <chalkerx@gmail.com>
2025-10-14 21:49:28 +0300

    doc: js fromBase64 rechecks last chunk

--- fallback/base64.js
+++ fallback/base64.js
@@ -94,7 +94,6 @@ export function toBase64(arr, isURL, padding) {
 // TODO: can this be optimized? This only affects non-Hermes barebone engines though
 const mapSize = nativeEncoder ? 256 : 65_536 // we have to store 64 KiB map or recheck everything if we can't decode to byte array
 
-// Last chunk is rechecked at API
 export function fromBase64(str, isURL) {
   let inputLength = str.length
   while (str[inputLength - 1] === '=') inputLength--
