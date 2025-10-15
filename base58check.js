6309e41dfbd577e9c91c26529876f5f0fe4c6cf0
Nikita Skovoroda <chalkerx@gmail.com>
2025-10-15 15:44:06 +0300

    feat: makeBase58Check

--- base58check.js
+++ base58check.js
@@ -6,25 +6,29 @@ import { hashSync } from '@exodus/crypto/hash'
 // Note: while API is async, we use hashSync for now until we improve webcrypto perf for hash256
 // Inputs to base58 are typically very small, and that makes a difference
 
-const hash256 = (x) => hashSync('sha256', hashSync('sha256', x, 'uint8'), 'uint8')
-
 const E_CHECKSUM = 'Invalid checksum'
 
-export async function toBase58check(arr) {
-  assertUint8(arr)
-  const checksum = hash256(arr)
-  const res = new Uint8Array(arr.length + 4)
-  res.set(arr, 0)
-  res.set(checksum.subarray(0, 4), arr.length)
-  return toBase58(res)
-}
+export const makeBase58Check = (hashAlgo) => ({
+  async encode(arr) {
+    assertUint8(arr)
+    const checksum = await hashAlgo(arr)
+    const res = new Uint8Array(arr.length + 4)
+    res.set(arr, 0)
+    res.set(checksum.subarray(0, 4), arr.length)
+    return toBase58(res)
+  },
+  async decode(str, format = 'uint8') {
+    const arr = fromBase58(str) // checks input
+    const len4 = arr.length - 4
+    const payload = arr.subarray(0, len4)
+    const c = arr.subarray(len4)
+    const r = await hashAlgo(payload)
+    if ((c[0] ^ r[0]) | (c[1] ^ r[1]) | (c[2] ^ r[2]) | (c[3] ^ r[3])) throw new Error(E_CHECKSUM)
+    return typedView(payload, format)
+  },
+})
 
-export async function fromBase58check(str, format = 'uint8') {
-  const arr = fromBase58(str) // checks input
-  const len4 = arr.length - 4
-  const payload = arr.subarray(0, len4)
-  const c = arr.subarray(len4)
-  const r = hash256(payload)
-  if ((c[0] ^ r[0]) | (c[1] ^ r[1]) | (c[2] ^ r[2]) | (c[3] ^ r[3])) throw new Error(E_CHECKSUM)
-  return typedView(payload, format)
-}
+const hash256 = (x) => hashSync('sha256', hashSync('sha256', x, 'uint8'), 'uint8')
+const { encode: toBase58check, decode: fromBase58check } = makeBase58Check(hash256)
+
+export { toBase58check, fromBase58check }
---
+++ tests/bs58check/base.test.js
@@ -0,0 +1,19 @@
+// From https://github.com/bitcoinjs/bs58check/tree/master/test
+
+import tape from '@exodus/test/tape'
+import { fromHex as hexToBytes } from '@exodus/bytes/hex.js'
+import { makeBase58Check } from '@exodus/bytes/base58check.js'
+import { hash } from '@exodus/crypto/hash'
+
+const blake256x2 = async (x) => hash('blake256', await hash('blake256', x, 'uint8'), 'uint8')
+const bs58check = makeBase58Check(blake256x2)
+
+tape('custom checksum function (blake256x2)', async function (t) {
+  const address = 'DsRLWShUQexhKE1yRdpe2kVH7fmULcEUFDk'
+  const payload = hexToBytes('073f0415e993935a68154fda7018b887c4e3fe8b4e10')
+
+  t.equal(await bs58check.encode(payload), address)
+  t.same(await bs58check.decode(address), payload)
+
+  t.end()
+})
