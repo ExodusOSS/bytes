b34456a1dfca208986cb9ec7cb37540baa8a2a92
Nikita Skovoroda <chalkerx@gmail.com>
2025-12-01 15:57:51 +0400

    bench: latin1 benchmarks

---
+++ benchmarks/latin1.bench.js
@@ -0,0 +1,76 @@
+import { utf8fromString } from '@exodus/bytes/utf8.js'
+import { benchmark } from '@exodus/test/benchmark' // eslint-disable-line @exodus/import/no-unresolved
+import buffer from 'buffer/index.js'
+import { describe, test } from 'node:test'
+
+import { bufs as bufsRaw } from './utils/random.js'
+import * as latin1 from '../fallback/latin1.js'
+
+if (!globalThis.Buffer) globalThis.Buffer = buffer.Buffer
+const bufferIsPolyfilled = Buffer === buffer.Buffer
+const toBuffer = (x, B) => B.from(x.buffer, x.byteOffset, x.byteLength)
+
+const replacementChar = String.fromCodePoint(0xff_fd) // We don't expect much of these in real usage, and rng will spawn a lot of those, so strip
+const strings = bufsRaw.map((x) => Buffer.from(x).toString().replaceAll(replacementChar, '∀')) // loose, but we want that here
+const bufs = strings.map((x) => utf8fromString(x))
+const latinStrings = bufs.map((x) => Buffer.from(x).toString('latin1'))
+
+const asciiBufs = bufsRaw.map((x) => x.map((c) => (c >= 0x80 ? c - 0x80 : c)))
+const asciiStrings = asciiBufs.map((x) => Buffer.from(x).toString())
+
+const isNative = (x) => x && (!bufferIsPolyfilled || `${x}`.includes('[native code]')) // we consider Node.js TextDecoder/TextEncoder native
+const { TextEncoder, btoa } = globalThis
+const textEncoder = isNative(TextEncoder) ? new TextEncoder() : null
+
+const timeout = 30_000
+describe('benchmarks: latin1', async () => {
+  // [name, impl, skip]
+  const asciiPrefix = [['./fallback/latin1', (x) => latin1.asciiPrefix(x)]]
+
+  // [name, impl, skip]
+  const decodeLatin1 = [
+    ['./fallback/latin1', (x) => latin1.decodeLatin1(x)],
+    ['Buffer', (x) => toBuffer(x, Buffer).toString('latin1')],
+    // ['Buffer.from', (x) => Buffer.from(x).toString('latin1')],
+    ['buffer/Buffer', (x) => toBuffer(x, buffer.Buffer).toString('latin1'), bufferIsPolyfilled],
+    // ['buffer/Buffer.from', (x) => buffer.Buffer.from(x).toString('latin1'), bufferIsPolyfilled],
+  ]
+
+  // [name, impl, skip]
+  const encodeLatin1 = [
+    ['./fallback/latin1', (x) => latin1.encodeLatin1(x)],
+    ['Buffer', (x) => Buffer.from(x, 'latin1')],
+    ['buffer/Buffer', (x) => buffer.Buffer.from(x, 'latin1'), bufferIsPolyfilled],
+    ['fromBase64 + btoa', (x) => Uint8Array.fromBase64(btoa(x)), !Uint8Array.fromBase64 || !btoa],
+  ]
+
+  // [name, impl, skip]
+  const encodeAscii = [
+    ['./fallback/latin1', (x) => latin1.encodeAscii(x, 'ERR'), !textEncoder],
+    ['TextEncoder', (x) => textEncoder.encode(x), !textEncoder],
+  ]
+
+  test('asciiPrefix', { timeout }, async () => {
+    for (const [name, f, skip] of asciiPrefix) {
+      await benchmark(`asciiPrefix: ${name}`, { skip, args: asciiBufs }, f)
+    }
+  })
+
+  test('decodeLatin1', { timeout }, async () => {
+    for (const [name, f, skip] of decodeLatin1) {
+      await benchmark(`decodeLatin1: ${name}`, { skip, args: bufs }, f)
+    }
+  })
+
+  test('encodeLatin1', { timeout }, async () => {
+    for (const [name, f, skip] of encodeLatin1) {
+      await benchmark(`encodeLatin1: ${name}`, { skip, args: latinStrings }, f)
+    }
+  })
+
+  test('encodeAscii', { timeout }, async () => {
+    for (const [name, f, skip] of encodeAscii) {
+      await benchmark(`encodeAscii: ${name}`, { skip, args: asciiStrings }, f)
+    }
+  })
+})
