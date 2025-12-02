a03d2ab8f4d6baec8b87c7ff3c32b8b61d3af65e
Nikita Skovoroda <chalkerx@gmail.com>
2025-12-02 06:38:34 +0400

    test: add encodeAscii tests

--- tests/latin1.test.js
+++ tests/latin1.test.js
@@ -1,4 +1,5 @@
-import { asciiPrefix } from '../fallback/latin1.js'
+import { asciiPrefix, encodeAscii } from '../fallback/latin1.js'
+import { nativeEncoder } from '../fallback/_utils.js'
 import { describe, test } from 'node:test'
 
 const raw = [
@@ -8,6 +9,7 @@ const raw = [
   Uint8Array.of(255),
   new Uint8Array(12_345),
   new Uint8Array(12_543).fill(255),
+  new Uint8Array(13_379).map((_, i) => i % 256),
 ]
 for (let i = 0; i < 5; i++) {
   const size = Math.floor(Math.random() * 1024)
@@ -16,7 +18,11 @@ for (let i = 0; i < 5; i++) {
 
 const pool = raw.map((uint8) => {
   const asciiBytes = uint8.map((x) => x & 0x7f)
-  return { uint8, asciiBytes }
+  const asciiString = Buffer.from(asciiBytes).toString()
+  const isAscii = uint8.every((x) => x < 0x80)
+  // uint8 -> string mapping could be corrupted by Buffer doing invalid conversion! isAscii is still correct for it though
+  const string = Buffer.from(uint8).toString()
+  return { uint8, asciiBytes, asciiString, isAscii, string }
 })
 
 describe('asciiPrefix', () => {
@@ -40,6 +46,24 @@ describe('asciiPrefix', () => {
         const pos = Math.floor(Math.random() * bytes.length)
         bytes[pos] |= 0x80
         t.assert.strictEqual(asciiPrefix(bytes), pos)
+      }
+    }
+  })
+})
+
+describe('encodeAscii', { skip: !nativeEncoder }, () => {
+  test('ascii', (t) => {
+    for (const { asciiBytes, asciiString } of pool) {
+      t.assert.deepStrictEqual(encodeAscii(asciiString), asciiBytes)
+    }
+  })
+
+  test('random', (t) => {
+    for (const { uint8, isAscii, string } of pool) {
+      if (isAscii) {
+        t.assert.deepStrictEqual(encodeAscii(string), uint8)
+      } else {
+        t.assert.throws(() => encodeAscii(string, 'ERR_MY'), /ERR_MY/)
       }
     }
   })
