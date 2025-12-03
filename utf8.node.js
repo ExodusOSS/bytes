20a82d9929b34898057532884119e175cf9d405a
Nikita Skovoroda <chalkerx@gmail.com>
2025-12-03 06:16:16 +0400

    perf: add Node.js-specific utf8.node.js

---
+++ utf8.node.js
@@ -0,0 +1,31 @@
+import { assertUint8 } from './assert.js'
+import { typedView } from './array.js'
+import { E_STRICT_UNICODE } from './fallback/utf8.js'
+import { isAscii } from 'node:buffer'
+
+const decoderFatal = new TextDecoder('utf8', { ignoreBOM: true, fatal: true })
+const decoderLoose = new TextDecoder('utf8', { ignoreBOM: true })
+const { isWellFormed } = String.prototype
+
+function encode(str, loose = false) {
+  if (typeof str !== 'string') throw new TypeError('Input is not a string')
+  const res = Buffer.from(str)
+  if (loose || str.length === res.length || isWellFormed.call(str)) return res // length is equal only for ascii, which is automatically fine
+  throw new TypeError(E_STRICT_UNICODE)
+}
+
+function decode(arr, loose = false) {
+  assertUint8(arr)
+  if (!loose && isAscii(arr)) {
+    // On non-ascii strings, this loses ~10% * [relative position of the first non-ascii byte] (up to 10% total)
+    // On ascii strings, this wins ~30% on loose = false and ~10% on loose = true
+    return Buffer.from(arr.buffer, arr.byteOffset, arr.byteLength).asciiSlice(0, arr.byteLength)
+  }
+
+  return loose ? decoderLoose.decode(arr) : decoderFatal.decode(arr)
+}
+
+export const utf8fromString = (str, format = 'uint8') => typedView(encode(str, false), format)
+export const utf8fromStringLoose = (str, format = 'uint8') => typedView(encode(str, true), format)
+export const utf8toString = (arr) => decode(arr, false)
+export const utf8toStringLoose = (arr) => decode(arr, true)
