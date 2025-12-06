7cd2686e3e3fea775772cd17d971165a5ff0f843
Nikita Skovoroda <chalkerx@gmail.com>
2025-12-06 13:56:38 +0400

    perf: speedup utf16 correctness check

--- fallback/utf16.js
+++ fallback/utf16.js
@@ -67,7 +67,27 @@ function encodeFatal(str, arr) {
 
 export function isWellFormed(u16) {
   const length = u16.length
-  for (let i = 0; i < length; i++) {
+  let i = 0
+
+  // Speedup with u32, by skipping to the first surrogate
+  // Only implemented for aligned input for now, but almost all input is aligned (pooled Buffer or 0 offset)
+  if (length > 32 && u16.byteOffset % 4 === 0) {
+    const u32length = (u16.byteLength / 4) | 0
+    const u32 = new Uint32Array(u16.buffer, u16.byteOffset, u32length)
+    for (const last3 = u32length - 3; ; i += 4) {
+      if (i >= last3) break // loop is fast enough for moving this here to be _very_ useful, likely due to array access checks
+      const a = u32[i]
+      const b = u32[i + 1]
+      const c = u32[i + 2]
+      const d = u32[i + 3]
+      if (a & 0x80_00_80_00 || b & 0x80_00_80_00 || c & 0x80_00_80_00 || d & 0x80_00_80_00) break
+    }
+
+    for (; i < u32length; i++) if (u32[i] & 0x80_00_80_00) break
+    i *= 2
+  }
+
+  for (; i < length; i++) {
     const code = u16[i]
     if (code >= 0xd8_00 && code < 0xe0_00) {
       // An unexpected trail or a lead at the very end of input
