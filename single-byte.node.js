6b977580c8973c20402c10db88896aa1fd137648
Nikita Skovoroda <chalkerx@gmail.com>
2025-12-08 12:31:32 +0400

    perf: faster single-byte on Deno

--- single-byte.node.js
+++ single-byte.node.js
@@ -1,13 +1,24 @@
 import { assertUint8 } from './assert.js'
 import { isAscii } from 'node:buffer'
 import { asciiPrefix, decodeLatin1 } from './fallback/latin1.js'
-import { encodingMapper } from './fallback/single-byte.js'
+import { encodingMapper, encodingDecoder } from './fallback/single-byte.js'
 
 const isLE = new Uint8Array(Uint16Array.of(258).buffer)[0] === 2
+const isDeno = Boolean(globalThis.Deno)
 const toBuf = (x) => Buffer.from(x.buffer, x.byteOffset, x.byteLength)
 
 export function createDecoder(encoding) {
   const mapper = encodingMapper(encoding) // asserts
+  if (isDeno) {
+    const jsDecoder = encodingDecoder(encoding)
+    return (arr) => {
+      assertUint8(arr)
+      if (arr.byteLength === 0) return ''
+      if (isAscii(arr)) return toBuf(arr).toString()
+      return jsDecoder(arr) // somewhy faster on Deno anyway, TODO: optimize?
+    }
+  }
+
   return (arr) => {
     assertUint8(arr)
     if (arr.byteLength === 0) return ''
