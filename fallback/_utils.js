5260b9073162066aee8c6f58139c2919fbfc7c2a
Nikita Skovoroda <chalkerx@gmail.com>
2025-12-08 06:58:42 +0400

    refactor: use direct exports

--- fallback/_utils.js
+++ fallback/_utils.js
@@ -1,15 +1,17 @@
 const { Buffer, TextEncoder, TextDecoder } = globalThis
 const haveNativeBuffer = Buffer && !Buffer.TYPED_ARRAY_SUPPORT
 const isNative = (x) => x && (haveNativeBuffer || `${x}`.includes('[native code]')) // we consider Node.js TextDecoder/TextEncoder native
-const nativeEncoder = isNative(TextEncoder) ? new TextEncoder() : null
-const nativeDecoder = isNative(TextDecoder) ? new TextDecoder('utf8', { ignoreBOM: true }) : null
-const nativeBuffer = haveNativeBuffer ? Buffer : null
-const isHermes = Boolean(globalThis.HermesInternal)
+export const nativeEncoder = isNative(TextEncoder) ? new TextEncoder() : null
+export const nativeDecoder = isNative(TextDecoder)
+  ? new TextDecoder('utf8', { ignoreBOM: true })
+  : null
+export const nativeBuffer = haveNativeBuffer ? Buffer : null
+export const isHermes = Boolean(globalThis.HermesInternal)
 
 // Actually windows-1252, compatible with ascii and latin1 decoding
 // Beware that on non-latin1, i.e. on windows-1252, this is broken in ~all Node.js versions released
 // in 2025 due to a regression, so we call it Latin1 as it's usable only for that
-const nativeDecoderLatin1 = isNative(TextDecoder)
+export const nativeDecoderLatin1 = isNative(TextDecoder)
   ? new TextDecoder('latin1', { ignoreBOM: true })
   : null
 
@@ -35,6 +37,4 @@ function shouldSkipBuiltins() {
   return false // eslint-disable-line no-unreachable
 }
 
-const skipWeb = shouldSkipBuiltins()
-
-export { nativeEncoder, nativeDecoder, nativeDecoderLatin1, nativeBuffer, isHermes, skipWeb }
+export const skipWeb = shouldSkipBuiltins()
