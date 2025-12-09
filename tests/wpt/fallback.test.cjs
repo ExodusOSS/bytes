49815de13542d63c33b399bdfeebd78f235f875e
Nikita Skovoroda <chalkerx@gmail.com>
2025-12-09 20:19:49 +0400

    test: don't attempt to run fallback on disabled

--- tests/wpt/fallback.test.cjs
+++ tests/wpt/fallback.test.cjs
@@ -13,6 +13,8 @@ const { loadDir } = require('./loader.cjs')
 const base64 = require('../../fallback/base64.js')
 const { TextEncoder, TextDecoder } = require('@exodus/bytes/text-encoding.js')
 
+if (!TextDecoder || !TextEncoder) throw new Error('No TextDecoder / TextEncoder')
+
 globalThis.TextEncoder = TextEncoder
 globalThis.TextDecoder = TextDecoder
 
