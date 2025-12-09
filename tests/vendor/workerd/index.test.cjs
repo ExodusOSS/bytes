6bc0d9f48bb67ba4a4a6715ef87f36b3c7eb0e65
Nikita Skovoroda <chalkerx@gmail.com>
2025-12-09 19:44:56 +0400

    test: import workerd TextEncoder/TextDecoder tests

---
+++ tests/vendor/workerd/index.test.cjs
@@ -0,0 +1,17 @@
+const { TextDecoder, TextEncoder } = require('@exodus/bytes/text-encoding.js')
+const { test, describe } = require('node:test')
+
+Object.assign(globalThis, { TextDecoder, TextEncoder })
+
+function run(title, tests) {
+  describe(title, () => {
+    for (const [name, obj] of Object.entries(tests)) {
+      test(name, async (t) => {
+        t.assert.deepStrictEqual(Object.keys(obj), ['test'])
+        obj.test()
+      })
+    }
+  })
+}
+
+run('encoding', require('./api/encoding_test.js'))
