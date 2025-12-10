9822f83aeb810a73e3d12f581e0a7003fac2bcc0
Nikita Skovoroda <chalkerx@gmail.com>
2025-12-10 23:15:33 +0400

    test: more TextDecoder snapshot tests

--- tests/text-encoding.bytes/text-encoding.bytes.test.js
+++ tests/text-encoding.bytes/text-encoding.bytes.test.js
@@ -68,7 +68,9 @@ describe('1-byte snapshot tests', () => {
 })
 
 const skipLarge =
-  process.env.EXODUS_TEST_PLATFORM === 'xs' || process.env.EXODUS_TEST_PLATFORM === 'engine262'
+  process.env.EXODUS_TEST_PLATFORM === 'quickjs' ||
+  process.env.EXODUS_TEST_PLATFORM === 'xs' ||
+  process.env.EXODUS_TEST_PLATFORM === 'engine262'
 
 describe('2-byte snapshot tests', { skip: skipLarge }, () => {
   for (const label of labels) {
@@ -81,7 +83,7 @@ describe('2-byte snapshot tests', { skip: skipLarge }, () => {
           const flags = []
           for (let i = 0; i < 256; i++) {
             u8[0] = i
-            for (let j = 0; j < 256; j++) {
+            for (let j = i; j < 256; j++) {
               const isAscii = i < 128 && j < 128
               if (isAscii !== (type === 'ascii')) continue
               u8[1] = j
@@ -183,6 +185,55 @@ describe('3-byte a-b-a snapshot tests', { skip: skipLarge }, () => {
               const isAscii = i < 128 && j < 128
               if (isAscii !== (type === 'ascii')) continue
               u8[1] = j
+
+              // See https://issues.chromium.org/issues/467624168, to cross-test with Chrome we want a clean decoder there
+              const loose = new TextDecoder(label)
+              const fatal = new TextDecoder(label, { fatal: true })
+              const a = loose.decode(u8)
+              t.assert.ok(a.length > 0, 1, `Bytes: ${i}, ${j}, ${j}`)
+              let b
+              let ok = false
+              try {
+                b = fatal.decode(u8)
+                ok = true
+              } catch {}
+
+              if (ok) t.assert.strictEqual(a, b, `Bytes: ${i}, ${j}, ${j}`)
+              const hex = [...a].map((c) => c.codePointAt(0).toString(16).padStart(4, '0')).join('')
+              flags.push(ok ? 1 : 0)
+              lengths.push(a.length)
+              out.push(hex)
+            }
+          }
+
+          if (t.assert.snapshot) {
+            t.assert.snapshot(toBase64(fromBits(flags.join(''))))
+            t.assert.snapshot(toBase64(fromBase4(lengths.join('')))) // 1-3
+            t.assert.snapshot(toBase64(fromHex(out.join(''))))
+          } else {
+            t.skip('Snapshots are not supported')
+          }
+        })
+      }
+    })
+  }
+})
+
+describe('3-byte a-a-b snapshot tests', { skip: skipLarge }, () => {
+  for (const label of labels) {
+    describe(label, () => {
+      const u8 = new Uint8Array(3)
+      for (const type of ['ascii', 'non-ascii']) {
+        test(type, (t) => {
+          const out = []
+          const flags = []
+          const lengths = []
+          for (let i = 0; i < 256; i++) {
+            u8[0] = u8[1] = i
+            for (let j = 0; j < 256; j++) {
+              const isAscii = i < 128 && j < 128
+              if (isAscii !== (type === 'ascii')) continue
+              u8[2] = j
 
               // See https://issues.chromium.org/issues/467624168, to cross-test with Chrome we want a clean decoder there
               const loose = new TextDecoder(label)
