d709d2bb3f026b20b22ac6869669380c7155a3d3
Nikita Skovoroda <chalkerx@gmail.com>
2025-12-14 05:40:45 +0400

    fix: detect chrome windows-1252 bug

--- single-byte.js
+++ single-byte.js
@@ -4,12 +4,35 @@ import { assertEncoding, encodingDecoder } from './fallback/single-byte.js'
 
 const { TextDecoder } = globalThis
 
+let windows1252works
+
+function shouldUseNative(enc) {
+  // https://issues.chromium.org/issues/468458388
+  // Also might be incorrectly imlemented on platforms as Latin1 (e.g. in Node.js) or regress
+  // This is the most significant single-byte encoding, 'ascii' and 'latin1' alias to this
+  // Even after Chrome bug is fixed, this should serve as a quick correctness check that it's actually windows-1252
+  if (enc === 'windows-1252') {
+    if (windows1252works === undefined) {
+      windows1252works = false
+      try {
+        const u = new Uint8Array(9) // using 9 bytes is significant to catch the bug
+        u[8] = 128
+        windows1252works = new TextDecoder(enc).decode(u).codePointAt(8) === 0x20_ac
+      } catch {}
+    }
+
+    return windows1252works
+  }
+
+  // iso-8859-16 is somehow broken in WebKit, at least on CI
+  return enc !== 'iso-8859-16'
+}
+
 export function createDecoder(encoding, loose = false) {
   if (encoding === 'iso-8859-8-i') encoding = 'iso-8859-8'
   assertEncoding(encoding)
 
-  // iso-8859-16 is somehow broken in WebKit, at least on CI
-  if (canDecoders && encoding !== 'iso-8859-16') {
+  if (canDecoders && shouldUseNative(encoding)) {
     // In try, as not all encodings might be implemented in all engines which have native TextDecoder
     try {
       const decoder = new TextDecoder(encoding, { fatal: !loose })
