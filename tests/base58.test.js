37369dd7bfb927077038cd388ac7cc902e30c3e8
Nikita Skovoroda <chalkerx@gmail.com>
2025-12-16 23:22:50 +0400

    test: more base58 vector tests

--- tests/base58.test.js
+++ tests/base58.test.js
@@ -1,7 +1,74 @@
-import { fromBase58, toBase58 } from '@exodus/bytes/base58.js'
+import { fromBase58, toBase58, fromBase58xrp, toBase58xrp } from '@exodus/bytes/base58.js'
+import { fromHex } from '@exodus/bytes/hex.js'
 import { randomValues } from '@exodus/crypto/randomBytes'
-import { test } from 'node:test'
+import { hashSync } from '@exodus/crypto/hash' // eslint-disable-line @exodus/import/no-deprecated
+import { describe, test } from 'node:test'
 import bs58 from 'bs58'
+import xAddressCodecFixtures from './vendor/x-address-codec/fixtures/base58.cjs'
+
+// https://en.bitcoin.it/wiki/Base58Check_encoding#Creating_a_Base58Check_string
+const toChecked = (version, pub) => {
+  const data = [Uint8Array.of(version), pub]
+  // eslint-disable-next-line @exodus/import/no-deprecated
+  const hashed = hashSync('sha256', hashSync('sha256', data, 'uint8'), 'uint8')
+  return Uint8Array.from(Buffer.concat([...data, hashed.subarray(0, 4)]))
+}
+
+describe('static vectors', () => {
+  test('base58', (t) => {
+    const fixtures = [
+      ['skip', fromHex('971a55')], // https://github.com/bitcoin/bitcoin/blob/13891a8a68/src/test/base58_tests.cpp
+    ]
+
+    for (const [string, u8] of fixtures) {
+      t.assert.deepStrictEqual(fromBase58(string), u8, string)
+      t.assert.strictEqual(toBase58(u8), string)
+    }
+  })
+
+  test('base58xrp', (t) => {
+    const fixtures = [
+      // https://xrpl.org/docs/tutorials/how-tos/use-specialized-payment-types/use-payment-channels#example-values
+      [
+        'aB44YfzW24VDEJQ2UuLPV2PvqcPCSoLnL7y5M1EzhdW4LnK5xMS3',
+        toChecked(
+          0x23, // https://xrpl.org/docs/references/protocol/data-types/base58-encodings
+          fromHex('023693F15967AE357D0327974AD46FE3C127113B1110D6044FD41E723689F81CC6')
+        ),
+      ],
+    ]
+
+    for (const [string, u8] of fixtures) {
+      t.assert.deepStrictEqual(fromBase58xrp(string), u8, string)
+      t.assert.strictEqual(toBase58xrp(u8), string)
+    }
+  })
+})
+
+describe('x-address-codec fixtures', () => {
+  test('base58', (t) => {
+    for (const { hex, string } of xAddressCodecFixtures.bitcoin) {
+      const u8 = fromHex(hex)
+      t.assert.strictEqual(toBase58(u8), string)
+      t.assert.deepStrictEqual(fromBase58(string), u8, string)
+    }
+  })
+
+  test('base58xrp', (t) => {
+    for (const { hex, string } of xAddressCodecFixtures.ripple) {
+      const u8 = fromHex(hex)
+      t.assert.strictEqual(toBase58xrp(u8), string)
+      t.assert.deepStrictEqual(fromBase58xrp(string), u8, string)
+    }
+  })
+
+  test('invalid', (t) => {
+    for (const { description, string } of xAddressCodecFixtures.invalid) {
+      t.assert.throws(() => fromBase58(string), description)
+      t.assert.throws(() => fromBase58xrp(string), description)
+    }
+  })
+})
 
 test('zeros', (t) => {
   for (let size = 0; size <= 1024; size++) {
---
+++ tests/vendor/x-address-codec/fixtures/base58.cjs
@@ -0,0 +1,2 @@
+// Fixture from https://github.com/ripple/x-address-codec/blob/master/test/fixtures/base58.json
+module.exports = require('./base58.json')
