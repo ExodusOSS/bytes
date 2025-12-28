87dbb870a9029dc3306de4dc024af50055354659
Nikita Skovoroda <chalkerx@gmail.com>
2025-12-29 00:53:39 +0400

    test: add iconv-lite based TextDecoder polyfill for testing iconv-lite

--- tests/encoding/_iconv_lite.js
+++ tests/encoding/_iconv_lite.js
@@ -1,5 +1,5 @@
-import { TextDecoder as TextDecoderPure } from '@exodus/bytes/encoding.js'
-import { decode, labelToName } from 'whatwg-encoding'
+import { labelToName, TextDecoder as TextDecoderPure } from '@exodus/bytes/encoding.js'
+import iconv from 'iconv-lite'
 
 // Very trivial polyfill to test _just_ the decoder output
 
@@ -7,7 +7,7 @@ export function TextDecoder(label, { fatal = false, ignoreBOM = false } = {}) {
   const encoding = labelToName(label) || label || 'UTF-8'
   return {
     decode(u8) {
-      const text = decode(u8, encoding)
+      const text = iconv.decode(u8, encoding, { stripBOM: !ignoreBOM })
 
       if (fatal && text.includes('\uFFFD')) {
         try {
