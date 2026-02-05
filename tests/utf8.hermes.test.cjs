51623bbe58a5e11e7279e27f2b187cadb8d63f7e
Nikita Skovoroda <chalkerx@gmail.com>
2026-02-05 04:17:17 +0400

    test: document XS issue (in code unused on XS)

--- tests/utf8.hermes.test.cjs
+++ tests/utf8.hermes.test.cjs
@@ -10,4 +10,12 @@ if (globalThis.HermesInternal) {
   globalThis.HermesInternal = true
 }
 
-require('./utf8.lib.test.js')
+if (process.env.EXODUS_TEST_PLATFORM === 'xs') {
+  // https://github.com/Moddable-OpenSource/moddable/issues/1562
+  // escape() path is used only on Hermes, not XS
+  // this test cross-tests that the Hermes path logic works on all engines
+  // but it doesn't on XS due to XS bug
+  require('node:test').test.skip('Known bug on XS escape(), path not used')
+} else {
+  require('./utf8.lib.test.js')
+}
