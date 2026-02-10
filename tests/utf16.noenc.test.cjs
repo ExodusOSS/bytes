346a4fa27da7c7530e0d6f6fd84d9b788f96cb42
Nikita Skovoroda <chalkerx@gmail.com>
2026-02-10 08:20:43 +0400

    size: add utf16.browser impl

--- tests/utf16.noenc.test.cjs
+++ tests/utf16.noenc.test.cjs
@@ -1,3 +1,7 @@
-delete globalThis.TextEncoder
-if (!globalThis.Deno) delete globalThis.TextDecoder // Deno path requires it
-require('./utf16.test.js')
+if (process.env.EXODUS_TEST_IS_BROWSER || globalThis.Deno) {
+  require('node:test').test.skip('Under browsers and Deno, TextDecoder is required')
+} else {
+  delete globalThis.TextEncoder
+  delete globalThis.TextDecoder
+  require('./utf16.test.js')
+}
