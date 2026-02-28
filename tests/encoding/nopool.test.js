8cf30fc9e0cb055017599613e33b9a3391057faa
Nikita Skovoroda <chalkerx@gmail.com>
2026-02-28 14:13:06 +0400

    test: isomorphicEncode returns non-pooled Uint8Array instances

--- tests/encoding/nopool.test.js
+++ tests/encoding/nopool.test.js
@@ -1,7 +1,7 @@
-import { TextEncoder } from '@exodus/bytes/encoding.js'
+import { TextEncoder, isomorphicEncode } from '@exodus/bytes/encoding.js'
 import { test } from 'node:test'
 
-test('TextEncoder returns non-pooled buffers', (t) => {
+test('TextEncoder returns non-pooled Uint8Array instances', (t) => {
   const encoder = new TextEncoder()
   for (let i = 0; i < 256; i++) {
     t.assert.strictEqual(encoder.encode('x'.repeat(128)).buffer.byteLength, 128)
@@ -17,5 +17,23 @@ test('TextEncoder returns non-pooled buffers', (t) => {
 
   for (let i = 0; i < 512; i++) {
     t.assert.strictEqual(encoder.encode('').buffer.byteLength, 0)
+  }
+})
+
+test('isomorphicEncode returns non-pooled Uint8Array instances', (t) => {
+  for (let i = 0; i < 256; i++) {
+    t.assert.strictEqual(isomorphicEncode('x'.repeat(128)).buffer.byteLength, 128)
+  }
+
+  for (let i = 0; i < 256; i++) {
+    t.assert.strictEqual(isomorphicEncode('x'.repeat(64)).buffer.byteLength, 64)
+  }
+
+  for (let i = 0; i < 512; i++) {
+    t.assert.strictEqual(isomorphicEncode('x'.repeat(32)).buffer.byteLength, 32)
+  }
+
+  for (let i = 0; i < 512; i++) {
+    t.assert.strictEqual(isomorphicEncode('').buffer.byteLength, 0)
   }
 })
